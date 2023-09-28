03c7b8e1e65904a9bdb6e33a2c1078b1c914a7f9
vivek22-7 <vivek22_7@live.com>
2023-09-29 00:49:45 +0530

    live fetch of news api added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
  
   render() {
-    let {title, description, imageUrl} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className='my-3'>
        <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://media.istockphoto.com/photos/news-button-picture-id598565916?k=6&m=598565916&s=612x612&w=0&h=OUSPGB0ZxA1AUzbG8kG0XboE-lb_HPYAyDQnuaP8Nzs=":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/" className="btn btn-sm btn-primary">Read More</a>
+                <p className="card-text">{description}...</p>
+                <a href={newsUrl}  className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
       </div>
