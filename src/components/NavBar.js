d9cda84ec02d09081ec7f425576f881aa56b4bbe
vivek22-7 <vivek22_7@live.com>
2023-10-06 00:48:45 +0530

    Date time and source badge added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,7 @@ export default class NavBar extends Component {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item"> <Link className="nav-link active" to="/">Home</Link></li>
+                    <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="/">Home</Link></li>
                     <li className="nav-item"> <Link className="nav-link" to="/business">Business</Link></li>
                     <li className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link></li>
                     <li className="nav-item"> <Link className="nav-link" to="/general">General</Link></li>
